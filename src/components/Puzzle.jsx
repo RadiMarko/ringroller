c0f1a6f59c61a56e34db9a6db0bf374771bf0795
RadiMarko <r.marko1995@gmail.com>
2025-08-26 15:15:04 +0200

    Add new ring pics

---
+++ src/components/Puzzle.jsx
@@ -0,0 +1,12 @@
+﻿export default function Puzzle() {
+    return (
+        <div className="puzzle-container">
+            <img className="ring1" src="/beach1.svg" alt="A ring of the puzzle"/>
+            <img className="ring2" src="/beach2.svg" alt="A ring of the puzzle"/>
+            <img className="ring3" src="/beach3.svg" alt="A ring of the puzzle"/>
+            <img className="ring4" src="/beach4.svg" alt="A ring of the puzzle"/>
+            <img className="ring5" src="/beach5.svg" alt="A ring of the puzzle"/>
+            <img className="ring6" src="/beach6.svg" alt="A ring of the puzzle"/>
+        </div>
+    )
+}
